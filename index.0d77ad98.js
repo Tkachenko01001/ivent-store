220120112ddb5b1ca24cc94c4d488714c143edd0
Tkachenko01001 <113258288+Tkachenko01001@users.noreply.github.com>
2023-06-13 19:07:57 +0000

    Deploying to gh-pages from @ Tkachenko01001/ivent-store@971c55bfdeb6c0975a3a96fdf2a6eefdbec3b5c5 🚀

---
+++ index.0d77ad98.js
@@ -0,0 +1,2 @@
+var o,l;o=5,l=5,console.log(o+l);
+//# sourceMappingURL=index.0d77ad98.js.map
---
+++ index.22fc417a.js
@@ -0,0 +1,2 @@
+!function(){var o,n;o=5,n=5,console.log(o+n)}();
+//# sourceMappingURL=index.22fc417a.js.map
